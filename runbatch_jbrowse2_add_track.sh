428665349ee5d7ceb7d73a2ffe32efe7709b0c1e
Colin Davenport <davenport.colin@mh-hannover.de>
2021-09-29 16:41:20 +0200

    Add categ to runbatch

--- runbatch_jbrowse2_add_track.sh
+++ runbatch_jbrowse2_add_track.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-## Colin Davenport, March 2021
+## Colin Davenport, March 2021 - Sept 2021
 
 echo "INFO: Add JBrowse2 tracks"
 
 # Change these as appropriate
 ref_asm=Rattus_norvegicus.Rnor_6.0.dna.toplevel
 subDir=Rnor6
+category_name="test"
 
 
 # Add BAMs
@@ -14,7 +15,7 @@ for i in `ls *bam`
 
         do
                 echo $i
-		bash addJBrowse2_track.sh $i $ref_asm  $subDir
+		bash addJBrowse2_track.sh $i $ref_asm  $subDir $category_name
 
 done
 
@@ -23,7 +24,7 @@ for i in `ls *gff.gz`
 
         do
                 echo $i
-		bash addJBrowse2_track.sh $i $ref_asm  $subDir
+		bash addJBrowse2_track.sh $i $ref_asm  $subDir $category_name
 
 done
 
@@ -32,8 +33,8 @@ for i in `ls *bed.gz`
 
         do
                 echo $i
-		bash addJBrowse2_track.sh $i $ref_asm  $subDir
+		bash addJBrowse2_track.sh $i $ref_asm  $subDir $category_name
 
 done
 
-
+echo "INFO: Done"
